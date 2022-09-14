9f774a69c55755ad4d12830ebb60621bb7d8a2c1
JanoKasu <109106614+JanoKasu@users.noreply.github.com>
2022-09-13 22:32:51 -0400

    Updated Requirements
    Added a requirements section outlining the objective of the project.
    Also began working on subroutines again.

--- Tasklist.md
+++ Tasklist.md
@@ -2,10 +2,30 @@
 
 ## Script
 
-### printBoard()
-### spaceIsFree()
-### isWinner()
-### playerTurn()
-### compTurn()
-### isBoardFull()
-### selectRandom()
+[x] printBoard()
+[x] spaceIsFree()
+[x] isWinner()
+[ ] playerTurn()
+[ ] compTurn()
+[ ] isBoardFull()
+[ ] selectRandom()
+
+## Requirements
+
+Introduce the game
+Print out a blank board
+{
+	Take input
+	Update board
+	Check for win
+	AI makes their move
+	Update board
+	Check for win
+}
+Repeat until out of moves or win is achieved
+Close game
+
+### Throwing Errors
+
+Out of bounds (0 < x < 2)
+Place already taken
--- TikTakToe/Program.cs
+++ TikTakToe/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace TikTakToe
+class Program
 {
-    class Program
+    public static void printBoard(char[] board)
     {
-        static void Main(string[] args)
+        Console.WriteLine("   |   |");
+        Console.WriteLine($" {board[1]} | {board[2]} | {board[3]}");
+        Console.WriteLine("   |   |");
+        Console.WriteLine("-----------");
+        Console.WriteLine("   |   |");
+        Console.WriteLine($" {board[4]} | {board[5]} | {board[6]}");
+        Console.WriteLine("   |   |");
+        Console.WriteLine("-----------");
+        Console.WriteLine("   |   |");
+        Console.WriteLine($" {board[7]} | {board[8]} | {board[9]}");
+        Console.WriteLine("   |   |");
+    }
+
+    public bool spaceIsFree(char[] board, int position)
+    {
+        if (board[position] == ' ') return true;
+        else return false;
+    }
+
+    public bool isWinner(char[] board, char letter)
+    {
+        char[] players = new char[] { 'X', 'O' }
+        foreach (char c in players)
         {
-            Console.WriteLine("Hello World!");
         }
+    }
+
+    public playerTurn()
+    {
+
+    }
+
+    static void Main(string[] args)
+    {
+        bool win = false;
+        char[] board = new char[10];
+        do
+        {
+            try
+            {
+                printBoard(board);
+                Console.WriteLine($"X's turn! Choose a number 1-9 (0 to quit).");
+                int x = Int32.Parse(Console.ReadLine());
+                if (x < 1 || x > 9) throw new IndexOutOfRangeException();
+                board[x] = 'X';
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        } while (!win);
     }
 }
